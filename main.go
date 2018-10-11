51929fde51e1345e931184b8e63121defc8961b1
Harry Scells <harryscells@gmail.com>
2018-10-11 17:30:11 +1000

    Complete more of the interface.

--- main.go
+++ main.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"github.com/boltdb/bolt"
 	"github.com/gin-gonic/gin"
 	"github.com/ielab/searchrefiner"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"path"
 )
 
 type IUserStudyPlugin struct{}
 
 type templating struct {
-	UID      string
-	Language string
+	UID       string
+	Language  string
+	Step      int
+	Protocol  int
+	Interface int
 }
 
 const (
-	bucketProgress = "progress"
+	bucketProgress  = "progress"
+	bucketInterface = "interface"
+	bucketProtocol  = "protocol"
 )
 
 var (
@@ -33,6 +41,15 @@ func (IUserStudyPlugin) Serve(s searchrefiner.Server, c *gin.Context) {
 		return
 	}
 
+	// Send the user to the tree interface if they are completing that task.
+	if _, ok := c.GetQuery("tree"); ok {
+		c.Render(http.StatusOK, searchrefiner.RenderPlugin(searchrefiner.TemplatePlugin("plugin/iuserstudy/queryvis.html"), templating{
+			UID:      uid,
+			Language: "pubmed",
+		}))
+		return
+	}
+
 	// Configure the database.
 	if db == nil {
 		var err error
@@ -42,25 +59,135 @@ func (IUserStudyPlugin) Serve(s searchrefiner.Server, c *gin.Context) {
 			c.AbortWithError(http.StatusUnauthorized, err)
 			return
 		}
+		err = db.Update(func(tx *bolt.Tx) error {
+			_, err := tx.CreateBucketIfNotExists([]byte(bucketProgress))
+			if err != nil {
+				return err
+			}
+			i, err := tx.CreateBucketIfNotExists([]byte(bucketInterface))
+			if err != nil {
+				return err
+			}
+			// check if bucket exists
+			err = i.Put([]byte(bucketInterface), []byte{byte(queryvis)})
+			if err != nil {
+				return err
+			}
+			p, err := tx.CreateBucketIfNotExists([]byte(bucketProtocol))
+			if err != nil {
+				return err
+			}
+			err = p.Put([]byte(bucketProtocol), []byte{byte(p1), 0})
+			return err
+		})
+		if err != nil {
+			c.HTML(http.StatusUnauthorized, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+			c.AbortWithError(http.StatusUnauthorized, err)
+			return
+		}
 	}
 
+	// Get the current progress of the user.
+	var step progress
 	err := db.View(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte(bucketProgress))
-		if err != nil {
-			return err
-		}
-		b.Put()
+		b := tx.Bucket([]byte(bucketProgress))
+		step = b.Get([]byte(uid))
+		return nil
 	})
 	if err != nil {
-		c.HTML(http.StatusUnauthorized, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
-		c.AbortWithError(http.StatusUnauthorized, err)
+		c.HTML(http.StatusNotFound, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+		c.AbortWithError(http.StatusNotFound, err)
 		return
 	}
 
+	// Handle what happens when the participant "completes" a step.
+	if c.Request.Method == "POST" {
+		stat, i, p := step.Get()
+		// Write the participants submitted query to the disk.
+		if stat == experiment1 || stat == experiment2 {
+			query := c.PostForm("query")
+			dir := path.Join("data", fmt.Sprint(i), fmt.Sprint(p))
+			err := os.MkdirAll(dir, os.ModePerm)
+			if err != nil {
+				c.HTML(http.StatusInternalServerError, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+			err = ioutil.WriteFile(path.Join(dir, uid), []byte(query), os.ModePerm)
+			if err != nil {
+				c.HTML(http.StatusInternalServerError, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+
+			// Upload the history file from PubMed.
+			if i == pubmed {
+				fh, err := c.FormFile("history")
+				if err != nil {
+					c.HTML(http.StatusInternalServerError, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+					c.AbortWithError(http.StatusInternalServerError, err)
+					return
+				}
+				f, err := fh.Open()
+				if err != nil {
+					c.HTML(http.StatusInternalServerError, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+					c.AbortWithError(http.StatusInternalServerError, err)
+					return
+				}
+				b, err := ioutil.ReadAll(f)
+				if err != nil {
+					c.HTML(http.StatusInternalServerError, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+					c.AbortWithError(http.StatusInternalServerError, err)
+					return
+				}
+				err = ioutil.WriteFile(path.Join(dir, fmt.Sprintf("%s_history.csv", uid)), b, os.ModePerm)
+				if err != nil {
+					c.HTML(http.StatusInternalServerError, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+					c.AbortWithError(http.StatusInternalServerError, err)
+					return
+				}
+			}
+		}
+
+		// Update the participant's interface and protocol.
+		u, err := step.Step([]byte(uid), db)
+		if err != nil {
+			c.HTML(http.StatusInternalServerError, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		stat, i, p = u.Get()
+
+		c.Redirect(http.StatusFound, fmt.Sprintf("/plugin/iuserstudy?uid=%s", uid))
+		return
+	}
+
+	// Initialise the participant with default values and commit them to the database.
+	if len(step) == 0 {
+		p, err := newProgress(db)
+		if err != nil {
+			c.HTML(http.StatusUnauthorized, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+			c.AbortWithError(http.StatusUnauthorized, err)
+			return
+		}
+		err = p.Update([]byte(uid))
+		if err != nil {
+			c.HTML(http.StatusNotFound, "error.html", searchrefiner.ErrorPage{Error: err.Error(), BackLink: "/"})
+			c.AbortWithError(http.StatusNotFound, err)
+			return
+		}
+		step = p
+	}
+
+	stat, i, p := step.Get()
+
 	// Respond to a regular request.
 	c.Render(http.StatusOK, searchrefiner.RenderPlugin(searchrefiner.TemplatePlugin("plugin/iuserstudy/index.html"), templating{
-		UID:      uid,
-		Language: "pubmed",
+		UID:       uid,
+		Step:      int(stat),
+		Interface: int(i),
+		Protocol:  int(p),
+		Language:  "pubmed",
 	}))
 }
 
--- progress.go
+++ progress.go
@@ -1,9 +1,24 @@
 package main
 
-import "github.com/boltdb/bolt"
+import (
+	"github.com/boltdb/bolt"
+	"github.com/go-errors/errors"
+)
 
 type progress []byte
-type status int
+type status byte
+type system byte
+type protocol byte
+
+const (
+	queryvis system = 1
+	pubmed   system = 2
+)
+
+const (
+	p1 protocol = 1
+	p2 protocol = 2
+)
 
 // 1. Pre-Experiment Questionnaire
 // 2. Pre-Task Questionnaire
@@ -13,22 +28,102 @@ type status int
 // 6. Interface 2 & Protocol 2
 // 7. Post-Task Questionnaire
 // 8. Post-Experiment Questionnaire
-const tasks = 8
-
 const (
-	complete status = iota
-	incomplete
+	preExperimentQuestionnaire  status = 1
+	preTaskQuestionnaire1              = 2
+	experiment1                        = 3
+	postTaskQuestionnaire1             = 4
+	preTaskQuestionnaire2              = 5
+	experiment2                        = 6
+	postTaskQuestionnaire2             = 7
+	postExperimentQuestionnaire        = 8
 )
 
-func newProgress() *progress {
-	p := make(progress, 1)
-	return &p
+var steps = []status{
+	preExperimentQuestionnaire,
+	preTaskQuestionnaire1,
+	experiment1,
+	postTaskQuestionnaire1,
+	preTaskQuestionnaire2,
+	experiment2,
+	postTaskQuestionnaire2,
+	postExperimentQuestionnaire,
 }
 
-func (p *progress) Step(user []byte, db *bolt.DB) {
-	[]byte(*p)[0]++
-	db.Update(func(tx *bolt.Tx) error {
+const protocols = 2
+const complete = 1
+
+func newProgress(db *bolt.DB) (progress, error) {
+	var iv, pv byte
+	err := db.Update(func(tx *bolt.Tx) error {
+		// Get and update the interface.
+		i := tx.Bucket([]byte(bucketInterface))
+		b1 := i.Get([]byte(bucketInterface))
+		iv = b1[0]
+		if b1[0] == byte(queryvis) {
+			b1 = []byte{byte(pubmed)}
+		} else {
+			b1 = []byte{byte(queryvis)}
+		}
+		i.Put([]byte(bucketInterface), b1)
+
+		// Get and update the protocol.
+		// First byte contains the protocol,
+		// second byte contains the number of assigned protocols so far.
+		p := tx.Bucket([]byte(bucketProtocol)) // Protocol bucket.
+		b2 := p.Get([]byte(bucketProtocol))    // Bytes for this key.
+		pv = b2[0]                             // Which protocol?
+		idx := b2[1]                           // Index of protocol assignment.
+		if idx >= protocols*2 {
+			b2 = []byte{byte(p2), idx}
+		} else if b2[1] == 0 {
+			b2 = []byte{byte(p1), idx}
+		}
+		// Note the increase of index.
+		return i.Put([]byte(bucketProtocol), []byte{b2[0], idx + 1})
+	})
+	return progress{byte(preExperimentQuestionnaire), byte(iv), byte(pv)}, err
+}
+
+func (p progress) Get() (status, system, protocol) {
+	return status(p[0]), system(p[1]), protocol(p[2])
+}
+
+func (p progress) Update(user []byte) error {
+	return db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucketProgress))
-		b.Put(user, *p)
+		err := b.Put(user, p)
+		return err
+	})
+}
+
+func (p progress) Step(user []byte, db *bolt.DB) (progress, error) {
+	// Do not progress if the experiments are complete.
+	if status(p[0]) == postExperimentQuestionnaire {
+		return p, errors.New("cannot progress past completion of study")
+	}
+
+	_, i, prot := p.Get()
+	// Update the interface and protocol for the next task.
+	if status(p[0]) == experiment1 {
+		if i == system(pubmed) {
+			i = system(queryvis)
+		} else {
+			i = system(pubmed)
+		}
+		if prot == protocol(p1) {
+			prot = protocol(p2)
+		} else {
+			prot = protocol(p1)
+		}
+	}
+
+	pr := progress{byte(status(p[0] + 1)), byte(i), byte(prot)}
+
+	// Then commit the step to the database.
+	return pr, db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketProgress))
+		err := b.Put(user, pr)
+		return err
 	})
 }
